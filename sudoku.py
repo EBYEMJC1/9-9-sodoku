55cb943c4bae8628957b8e4a748fed62b1fbd590
EBYEMJC1 <42852061+EBYEMJC1@users.noreply.github.com>
2023-05-16 11:36:51 -0700

    Create sudoku.py

---
+++ sudoku.py
@@ -0,0 +1,19 @@
+import math
+from math import sqrt
+
+
+while True:
+    try:
+        number_of_rows_columns = int(input("Enter a perfect square as the size of your board: "))
+        sqrt = math.sqrt(int(number_of_rows_columns))
+        is_it_perfect_square = int(sqrt + .5) ** 2
+        if int(number_of_rows_columns) != int(is_it_perfect_square):
+            raise ValueError(f"{int(number_of_rows_columns)} is not a perfect square")
+        break
+    except ValueError:
+        print("Invalid input please input a perfec square")
+
+
+rows_columns = [[0]*number_of_rows_columns for i in range(number_of_rows_columns)]
+for rows in rows_columns:
+    print(" ".join(str(x) for x in rows))
